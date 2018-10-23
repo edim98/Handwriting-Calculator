a941c687ae7d3dcda1702e5f6eb3ab43b4a9fb6b
s1988077 <b.m.ouderoelink@student.utwente.nl>
2018-10-23 17:13:09 +0000

    Cleanup and (performance) optimisation of Formula_string_to_array.py

--- Formula_string_to_array.py
+++ Formula_string_to_array.py
@@ -1,5 +1,8 @@
 import re
 
+SIGNS_1 = ('*', '/')
+SIGNS_2 = ('+', '-')
+
 #change to communicate to fpga
 def fpga_calc(num1, sign, num2):
 	string = str(num1)
@@ -8,46 +11,35 @@ def fpga_calc(num1, sign, num2):
 	integer = int(eval(string))
 	return integer
 
+def get_calcs(start_array, signs):
+	end_array = []
+	for i in range(0, len(start_array)):
+		if start_array[i] in signs:
+			end_array.pop(len(end_array)-1)
+			start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
+		else:
+			end_array.append(start_array[i])
+	return end_array
+
+def formula_to_array(formula):
+	temp = ""
+	array = []
+	for character in range (0, len(formula)):
+		if re.match(r"[0-9]", formula[character]):
+			temp += str(formula[character])
+		else:
+			if len(temp) != 0:
+				array.append(int(temp))
+				temp = ""
+			array.append(formula[character])
+	if len(temp) != 0:
+		array.append(int(temp))
+	return array
+
+def get_formula_answer(formula):
+	return get_calcs(get_calcs(formula_to_array(formula), SIGNS_1), SIGNS_2)[0]
+
+
+
 formula = "245*22/11+33/11-1"
-
-array = []
-temp = ""
-
-for character in range (0, len(formula)):
-	if re.match(r"[0-9]", formula[character]):
-		temp += str(formula[character])
-	else:
-		if len(temp) != 0:
-			array.append(int(temp))
-			temp = ""
-		array.append(formula[character])
-if len(temp) != 0:
-	array.append(int(temp))
-
-start_array = array
-end_array = []
-
-for i in range(0, len(start_array)):
-	if start_array[i] == '*':
-		end_array.pop(len(end_array)-1)
-		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
-	elif start_array[i] == '/':
-		end_array.pop(len(end_array)-1)
-		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
-	else:
-		end_array.append(start_array[i])
-
-start_array = end_array
-end_array = []
-
-for i in range(0, len(start_array)):
-	if start_array[i] == '+':
-		end_array.pop(len(end_array)-1)
-		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
-	elif start_array[i] == '-':
-		end_array.pop(len(end_array)-1)
-		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
-	else:
-		end_array.append(start_array[i])
-
-print(end_array[0])
+print(get_formula_answer(formula))
