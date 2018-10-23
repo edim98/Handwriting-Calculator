81459208525a90a49f535c9c0351eaa97610a998
s1988077 <b.m.ouderoelink@student.utwente.nl>
2018-10-23 15:02:12 +0000

    Added translating array into calculations to Formula_string_to_array.py

--- Formula_string_to_array.py
+++ Formula_string_to_array.py
@@ -1,12 +1,19 @@
 import re
 
-formula = "245+33"
+#change to communicate to fpga
+def fpga_calc(num1, sign, num2):
+	string = str(num1)
+	string += str(sign)
+	string += str(num2)
+	integer = int(eval(string))
+	return integer
+
+formula = "245*22/11+33/11-1"
 
 array = []
 temp = ""
 
 for character in range (0, len(formula)):
-	print(formula[character])
 	if re.match(r"[0-9]", formula[character]):
 		temp += str(formula[character])
 	else:
@@ -17,5 +24,30 @@ for character in range (0, len(formula)):
 if len(temp) != 0:
 	array.append(int(temp))
 
-print(array)
+start_array = array
+end_array = []
 
+for i in range(0, len(start_array)):
+	if start_array[i] == '*':
+		end_array.pop(len(end_array)-1)
+		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
+	elif start_array[i] == '/':
+		end_array.pop(len(end_array)-1)
+		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
+	else:
+		end_array.append(start_array[i])
+
+start_array = end_array
+end_array = []
+
+for i in range(0, len(start_array)):
+	if start_array[i] == '+':
+		end_array.pop(len(end_array)-1)
+		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
+	elif start_array[i] == '-':
+		end_array.pop(len(end_array)-1)
+		start_array[i+1] = fpga_calc(start_array[i-1], start_array[i], start_array[i+1])
+	else:
+		end_array.append(start_array[i])
+
+print(end_array[0])
