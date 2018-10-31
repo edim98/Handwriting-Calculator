21e378977bb817b8829c5a30db6050ce03688748
s1988077 <b.m.ouderoelink@student.utwente.nl>
2018-10-31 11:03:33 +0000

    actually checking for negative overflow in input now

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,7 @@ SIGNS_3 = ('(', ')')
 
 def reset_pins_to_low():
 	for pin in range(2, 13):
-	GPIO.output(pin, GPIO.LOW)
+		GPIO.output(pin, GPIO.LOW)
 
 def send_instruction(num1, sign, num2):
 	acknowledged = False
@@ -17,7 +17,7 @@ def send_instruction(num1, sign, num2):
 	solution_int = 0
 
 	#check for overflow before sending
-	if num1>1023 or num2>1023:
+	if num1 > 1023 or num2 > 1023 or num1 < -1024 or num2 < -1024:
 		return "Input number too high"
 		break
 
