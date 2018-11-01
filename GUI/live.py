897b932d42807defc2dc774802300f9c58316eba
Andrei Popa <a.popa@student.utwente.nl>
2018-11-01 15:17:00 +0100

    GUI

--- GUI/live.py
+++ GUI/live.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-from PIL import Image, ImageDraw
-from PIL import ImageTk
-import tkinter as tki
+from tkinter import *
+from PIL import ImageTk, Image
+
 import threading
 import datetime
 import imutils
@@ -19,20 +19,22 @@ class Live:
         self.thread = None
         self.stopEvent = None
 
-        self.root = tki.Tk()
+        self.root = Tk()
+        self.root.geometry("820x800")
         self.root.resizable(width = False, height = False)
 
         self.panel = None
 
         # btn = tki.Button(self.root, text = "Capture operation", command = self.captureImage)
         # btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=5)
-        canvas = tki.Canvas(self.root, height = 80)
-        canvas.pack(side = "bottom", fill = "both", expand = "yes", padx = 10)
-        x1 = 2
-        y1 = 2
-        x2 = 800
-        y2 = 80
-        radius = 25
+
+        self.canvas = Canvas(self.root, height = 90)
+        self.canvas.pack(side = "bottom", fill = "both", expand = "yes", padx = 5, pady = 5)
+        x1 = 5
+        y1 = 5
+        x2 = 805
+        y2 = 85
+        radius = 360
         points = [x1 + radius, y1,
                   x1 + radius, y1,
                   x2 - radius, y1,
@@ -53,10 +55,11 @@ class Live:
                   x1, y1 + radius,
                   x1, y1 + radius,
                   x1, y1]
-
-        canvas.create_polygon(points, fill = "#cd00cd", smooth=True)
-        canvas.create_text(100, 20, fill = "black", text = "Hello world")
-
+        self.img = Image.open(os.path.abspath("ProjectImages\image.jpg")).resize((800, 600), Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(self.img)
+        self.canvas.create_image(0, 0, image = self.image)
+        self.canvas.create_polygon(points, fill = "#c7261a", smooth=True, outline = "black", width = 1)
+        self.canvas.create_text(405, 30, fill = "black", text = "This is the result:", font = ("Engravers MT", 10))
 
         self.stopEvent = threading.Event()
         self.thread = threading.Thread(target=self.videoLoop, args=())
@@ -64,6 +67,8 @@ class Live:
 
         self.root.wm_title("BARBIE")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+        self.root.update_idletasks()
+        self.root.update()
 
 
 
@@ -72,16 +77,16 @@ class Live:
         try:
             while not self.stopEvent.is_set():
                 self.frame = self.vs.read()
-                self.frame = imutils.resize(self.frame, width=800)
+                self.frame = imutils.resize(self.frame, width=820)
 
                 image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
                 image = Image.fromarray(image)
                 image = ImageTk.PhotoImage(image)
 
                 if self.panel is None:
-                    self.panel = tki.Label(image = image)
+                    self.panel = Label(image = image)
                     self.panel.image = image
-                    self.panel.pack(side="left", padx = 10, pady = 5)
+                    self.panel.pack()
 
                 else:
                     self.panel.configure(image = image)
@@ -92,6 +97,7 @@ class Live:
 
     def onClose(self):
         print("[INFO] closing...")
+        self.thread.join()
         self.stopEvent.set()
         self.vs.stop()
         self.root.quit()
